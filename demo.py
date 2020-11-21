a05b198aa981c97dc5f6a8d6b35ece5849da22c0
Sicheng Marc SUN <sun9703@student.ubc.ca>
2020-11-21 02:47:02 -0800

    demo.py add main

--- demo.py
+++ demo.py
@@ -6,12 +6,18 @@ Usage: docopt.py <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
+[<arg4>]          Takes any value (this is an optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 """
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt['<arg4>'])
-print(opt)
-print(type(opt))
+
+def main(arguments):
+    print(arguments['<arg4>'])
+    print(arguments)
+    print(type(arguments))
+    
+if __name__ == "__main__":
+    main(opt)
